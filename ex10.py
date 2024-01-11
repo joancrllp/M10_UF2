738c879edafcada041cef1e3078841cfce56c967
Joan Cruz <2023_joan.cruz@iticbcn.cat>
2024-01-11 18:27:31 +0100

    a

---
+++ ex10.py
@@ -0,0 +1,1 @@
+#Crear un arxiu on caldrà endevinar el número escollit pel programa entre 1 i 100. Cada vegada que l’usuari hi posi un número, caldrà indicar si és més petit o més gran fins que encerti i el missatge haurà d’indicar que ha encertat. Indicar també el número d’intents.
